47595d159af480f3b4e739ab141312ef17bf9ee5
Anoop Singh <Reggerfgod@gmail.com>
2026-01-03 21:31:48 +0530

    Fix

--- common/src/main/java/com/reggarf/mods/underground_village/config/USConfigs.java
+++ common/src/main/java/com/reggarf/mods/underground_village/config/USConfigs.java
@@ -1,46 +1,35 @@
 package com.reggarf.mods.underground_village.config;
 
 import com.reggarf.mods.underground_village.Underground_village_Common;
-import me.shedaniel.autoconfig.ConfigData;
-import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
-import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
+import com.reggarf.mods.better_lib.config.annotation.Config;
+import com.reggarf.mods.better_lib.config.annotation.ConfigEntry.*;
+import com.reggarf.mods.better_lib.config.api.ConfigData;
 
-@Config(name = Underground_village_Common.MODID)
-@Config.Gui.Background("minecraft:textures/block/mossy_cobblestone.png")
-public class USConfigs extends PartitioningSerializer.GlobalData {
+@Config(modid = Underground_village_Common.MODID,name = "common")
+public class USConfigs implements ConfigData {
 
-    @ConfigEntry.Category("common")
-    @ConfigEntry.Gui.TransitiveObject()
-    public Common common = new Common();
+    @Category("Generation")
+    @Description("Default size of underground village structures.")
+    @BoundedDiscrete(min = 1, max = 30)
+    public int structureSize = 15;
 
-    @Config(name = "common")
-    public static final class Common implements ConfigData {
+    @Category("Generation")
+    @Description("Maximum distance structure pieces can generate from the village center.")
+    @BoundedDiscrete(min = 1, max = 128)
+    public int maxDistanceFromCenter = 116;
 
-        @ConfigEntry.Gui.Tooltip
-        @ConfigEntry.BoundedDiscrete(min = 1, max = 30)
-        @Comment("Default size of the underground structure (from 1 to 30)")
-        public int structureSize = 15;
+    @Category("Water Structures")
+    @Description("Default size of water-based underground village structures.")
+    @BoundedDiscrete(min = 1, max = 30)
+    public int waterStructureSize = 15;
 
-        @ConfigEntry.Gui.Tooltip
-        @ConfigEntry.BoundedDiscrete(min = 1, max = 128)
-        @Comment("Maximum distance the structure pieces can be from the center (1 to 128)")
-        public int maxDistanceFromCenter = 116;
+    @Category("Water Structures")
+    @Description("Maximum distance water structure pieces can generate from the center.")
+    @BoundedDiscrete(min = 1, max = 128)
+    public int waterMaxDistanceFromCenter = 116;
 
-        @ConfigEntry.Gui.Tooltip
-        @ConfigEntry.BoundedDiscrete(min = 1, max = 30)
-        @Comment("Default size of the underground structure (from 1 to 30)")
-        public int waterstructureSize = 15;
+    @Category("Messages")
+    @Description("Show the welcome message with Discord, GitHub, and support links on first join. (Requires restart)")
+    public boolean enableInGameMessage = true;
 
-        @ConfigEntry.Gui.Tooltip
-        @ConfigEntry.BoundedDiscrete(min = 1, max = 128)
-        @Comment("Maximum distance the structure pieces can be from the center (1 to 128)")
-        public int watermaxDistanceFromCenter = 116;
-
-        @ConfigEntry.Gui.Tooltip
-        @Comment("Enable or disable the welcome message, Requires game restart after changing this setting.")
-        public Boolean ENABLE_IN_GAME_MESSAGE = true;
-    }
 }
-
